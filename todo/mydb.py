11407e126854c24d51d75e6cb3be2aacd9ff5133
박종우 <indralight79@gmail.com>
2022-08-23 10:54:55 +0900

    drf-training > todo

---
+++ todo/mydb.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "USER": "root",
+        "PASSWORD": "12345678",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "NAME": "django_todo",
+    }
+}
---
+++ todo/todo/views.py
@@ -0,0 +1,57 @@
+from rlcompleter import Completer
+from django.shortcuts import (
+    render,
+    get_list_or_404,
+    redirect,
+)
+
+from todo.forms import TodoForm
+from .models import Todo
+
+# Create your views here.
+def todo_list(request):
+    # todos = get_list_or_404(Todo, complete=False)
+    todos = Todo.objects.filter(complete=False)
+    return render(request, "todo/todo_list.html", {"todos": todos})
+
+
+def todo_detail(request, pk):
+    todo = Todo.objects.get(id=pk)
+    return render(request, "todo/todo_detail.html", {"todo": todo})
+
+
+def todo_post(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect("todo_list")
+    else:
+        form = TodoForm()
+    return render(request, "todo/todo_post.html", {"form": form})
+
+
+def todo_edit(request, pk):
+    todo = Todo.objects.get(id=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect("todo_list")
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, "todo/todo_post.html", {"form": form})
+
+
+def todo_done(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.complete = True
+    todo.save()
+    return redirect("todo_list")
+
+
+def done_list(request):
+    dones = Todo.objects.filter(complete=True)
+    return render(request, "todo/done_list.html", {"dones": dones})
